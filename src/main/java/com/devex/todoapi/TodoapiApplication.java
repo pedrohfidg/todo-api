854987e237c58335d608f3cc3449fc140a11eecd
PedroHFG <pedrohfidg@gmail.com>
2025-11-07 10:38:21 -0300

    chore: configura conexão PostgreSQL, banco de dados H2 e configurações JPA

--- src/main/java/com/devex/todoapi/TodoapiApplication.java
+++ src/main/java/com/devex/todoapi/TodoapiApplication.java
@@ -1,5 +1,6 @@
 package com.devex.todoapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoapiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TodoapiApplication.class, args);
 	}
 
