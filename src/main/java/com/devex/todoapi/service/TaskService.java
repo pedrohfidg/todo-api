3a5dad429f6a124f3ced52acbe1bba1b87acb386
PedroHFG <pedrohfidg@gmail.com>
2025-11-07 09:14:33 -0300

    feat: implementa TaskService utilizando DTOs e regras de negócio

--- src/main/java/com/devex/todoapi/service/TaskService.java
+++ src/main/java/com/devex/todoapi/service/TaskService.java
@@ -2,11 +2,14 @@ package com.devex.todoapi.service;
 
 import com.devex.todoapi.dto.TaskRequestDTO;
 import com.devex.todoapi.dto.TaskResponseDTO;
+import com.devex.todoapi.model.Task;
+import com.devex.todoapi.model.TaskStatus;
 import com.devex.todoapi.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -17,26 +20,57 @@ public class TaskService {
 
     @Transactional(readOnly = true)
     public List<TaskResponseDTO> findAll() {
-        return null;
+        List<Task> result = taskRepository.findAll();
+        return result.stream().map(x -> new TaskResponseDTO(x)).toList();
     }
 
     @Transactional(readOnly = true)
     public TaskResponseDTO findById(Long id) {
-        return null;
+        Task task = taskRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Task de id " + id + " não encontrado")
+        );
+        return new TaskResponseDTO(task);
     }
 
     @Transactional
     public TaskResponseDTO create(TaskRequestDTO requestDTO) {
-        return null;
+        Task task = new Task();
+        copyDtoToEntity(requestDTO, task);
+        task.setStatus(TaskStatus.PENDENTE);
+        task.setDataCriacao(LocalDateTime.now());
+        task = taskRepository.save(task);
+
+        return new TaskResponseDTO(task);
     }
 
     @Transactional
     public TaskResponseDTO update(Long id, TaskRequestDTO requestDTO) {
-        return null;
+        try {
+            Task task = taskRepository.getReferenceById(id);
+            copyDtoToEntity(requestDTO, task);
+            task = taskRepository.save(task);
+            return new TaskResponseDTO(task);
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Task de id " + id + " não encontrado");
+        }
     }
 
     @Transactional
     public void delete(Long id) {
+        if (!taskRepository.existsById(id)) {
+           throw new RuntimeException("Task de id " + id + " não encontrado");
+        }
+        taskRepository.deleteById(id);
+    }
 
+    private void copyDtoToEntity(TaskRequestDTO requestDTO, Task task) {
+        if (requestDTO.getTitulo() != null) {
+                task.setTitulo(requestDTO.getTitulo());
+            }
+
+            if (requestDTO.getDescricao() != null) {
+                task.setDescricao(requestDTO.getDescricao());
+            }
     }
 }
